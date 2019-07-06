4a52b39cb52afc0b4ac37f5f02cf0e20a246fc45
Omega <fuhm@raysdate.com>
2019-07-06 20:41:42 +0800

    '122'

---
+++ wordCount.py
@@ -0,0 +1,9 @@
+#-*- coding:utf8-*-
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("WordCount").setMaster("local")
+sc = SparkContext(conf=conf)
+inputFile =  "file://home/linziyu/word.txt"
+textFile = sc.textFile(inputFile)
+wordCount = textFile.flatMap(lambda line : line.split(" ")).map(lambda word : (word, 1)).reduceByKey(lambda a, b : a + b)
+wordCount.foreach(print)
